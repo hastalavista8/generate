1ab42a3d7674272cb8d9a0bc54c2342f0a6302d9
HASTALAVISTA <48452238+hastalavista8@users.noreply.github.com>
2024-09-06 13:12:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $urls = [
     'https://megaforwinbet.xyz',
     'https://nasgorbetseafood.store',
     'https://nagaforwinapi.xyz',
+    'https://buditogeljitu.xyz',
     // Add more URLs here
 ];
 
